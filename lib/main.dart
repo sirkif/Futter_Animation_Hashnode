82ec3e206fc20e582cc25e08dcf5ed0183b443ad
adamkif <adam.sirkif@gmail.com>
2021-08-31 20:47:35 +0100

    minor fix

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter name',
+      title: 'Flutter Animation',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
